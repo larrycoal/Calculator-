5cedf686e01a692ae03c26f6c2552c8795815470
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2019-11-25 12:18:53 +0100

    first react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component}from 'react';
+import Button from './components/Button';
+import  "./css/style.css"
 
-function App() {
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      current:'0',
+      previous:[],
+      nextisReset:false
+    }
+  }
+ reset = ()=>{
+    this.setState({current:'0',previous:[]})
+  }
+
+  addToCurrent =(symbol)=>{
+    if(['+','*','-','/'].indexOf(symbol)>-1){
+      let {previous} = this.state
+      previous.push(this.state.current + symbol)
+      this.setState({previous, nextisReset:true})
+    }
+    if((this.state.current ==='0' && symbol !== ".")||this.state.nextisReset){
+     this.setState({current: symbol,nextisReset:false})
+  }else{
+    this.setState({current:this.state.current + symbol})
+  }
+}
+
+calculate =()=>{
+  let {previous,current,nextisReset}=this.state
+  current=eval(String(previous + current))
+  this.setState({current,previous:[],nextisReset:true})
+}
+ render(){
+   const Buttons = [
+     {symbol:'C',cols:'3',action:this.reset},
+     {symbol:'/',cols:'1',action:this.addToCurrent},
+     {symbol:'7',cols:'1',action:this.addToCurrent},
+     {symbol:'8',cols:'1',action:this.addToCurrent},
+     {symbol:'9',cols:'1',action:this.addToCurrent},
+     {symbol:'+',cols:'1',action:this.addToCurrent},
+     {symbol:'4',cols:'1',action:this.addToCurrent},
+     {symbol:'6',cols:'1',action:this.addToCurrent},
+     {symbol:'5',cols:'1',action:this.addToCurrent},
+     {symbol:'-',cols:'1',action:this.addToCurrent},
+     {symbol:'1',cols:'1',action:this.addToCurrent},
+     {symbol:'2',cols:'1',action:this.addToCurrent},
+     {symbol:'3',cols:'1',action:this.addToCurrent},
+     {symbol:'*',cols:'1',action:this.addToCurrent},
+     {symbol:'0',cols:'2',action:this.addToCurrent},
+     {symbol:'.',cols:'1',action:this.addToCurrent},
+     {symbol:'=',cols:'1',action:this.calculate},
+
+   ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='floaty'>
+         {this.state.previous[this.state.previous.length-1]}
+      </div>
+     <input className="result" type="text" value={this.state.current}/>
+     <br/>
+     {Buttons.map((btn,i)=>{
+       return <Button key={i} symbol = {btn.symbol} cols ={btn.cols} action={(symbol)=>btn.action(symbol)}/>
+     })}
     </div>
   );
+ }
+ 
 }
 
 export default App;
